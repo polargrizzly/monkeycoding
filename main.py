88f5b7ae99624c2373cd1edad22d9ae136bbee98
polargrizzly <viktorblon@gmail.com>
2023-03-12 00:56:42 +0200

    вніс зміни 0:56

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ print("monkeyCoding")
 # OK, Let's check how it works
 
 print("sent the repository link with main.py @ 00:33 (GMT+2)")
+
+name = input("Type your name: ")
+print("Glad to meet you,",name,"!")
+if name == 'IGOR' or name == 'Igor' or name == "igor":
+    print("Nice ass!")
+
