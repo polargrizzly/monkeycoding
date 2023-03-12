7b88db0f07ef9ab8e0c9469f1969978b3ba509b0
polargrizzly <viktorblon@gmail.com>
2023-03-12 14:14:16 +0200

    вніс зміни 0:56

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-print("monkeyCoding")
-print("@")
-print("monkeyCoding")
-# OK, Let's check how it works
-
-print("sent the repository link with main.py @ 00:33 (GMT+2)")
-
-name = input("Type your name: ")
-print("Glad to meet you,",name,"!")
-if name == 'IGOR' or name == 'Igor' or name == "igor":
-    print("Nice ass!")
+monkey = [17, 9, 8, 2]
 
