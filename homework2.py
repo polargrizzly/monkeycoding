8af3215b8a3717c95b5a962064c4c6962da35023
polargrizzly <viktorblon@gmail.com>
2023-03-18 03:00:40 +0200

    пробую запрінтити квадрат 4х4 із символа "+", квадрат 4х4 з "+" через range()

--- homework2.py
+++ homework2.py
@@ -1,3 +1,7 @@
-print('+ + + + \n+ + + + \n+ + + + \n+ + + +')
+print('+ + + + \n+ + + + \n+ + + + \n+ + + + \n')
 # \n = newline \t = space \r = backspace
 
+for y in range(4):
+    for x in range(4):
+        print("+", end="")
+    print()
